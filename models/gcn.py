8acc553d38d41aa043b06bf37cc4b908a40c3001
Paula Demmelmayr <paula.demmel@outlook.de>
2025-11-13 14:48:54 +0100

    implemented basic GCN and MLP model

---
+++ models/gcn.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class GCN(nn.Module):
+    """
+    2-layer Graph Convolutional Network (GCN) after the paper by Kipf et al.
+    (https://github.com/senadkurtisi/pytorch-GCN/tree/main/data)
+    """
+
+    def __init__(self, in_dim: int, hidden_dim:int, out_dim: int, droput, use_bias=True):
+        super(GCN,self).__init__()
+        self.in_dim = in_dim
+        self.out_channels = out_dim
+        self.use_bias = use_bias
+
+        self.conv1 = GCNConv(in_dim, hidden_dim, use_bias)
+        self.conv2 = GCNConv(hidden_dim, out_dim, use_bias)
+        self.dropout = nn.Dropout(p=droput)
+
+    def forward(self, x, edge_index ):
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.dropout(x)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
---
+++ models/mlp.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MLP(nn.Module):
+    """
+    Multi-layer perceptron (MLP) for only using node features and ignores
+    graph-structure
+    """
+
+    def __init__(self, in_dim:int, hidden_dim: int, out_dim:int, dropout:float):
+        super(MLP, self).__init__()
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.out_dim = out_dim
+
+        self.lin1 = nn.Linear(in_dim, hidden_dim)
+        self.lin2 = nn.Linear(hidden_dim, out_dim)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
