9bcd681277767b5cf2a6a9604120e4fba8889b85
Paula Demmelmayr <paula.demmel@outlook.de>
2025-11-25 16:38:21 +0100

    fixed paths

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,18 +7,19 @@ from pathlib import Path
 from torch_geometric.data import Data
 from sklearn.preprocessing import LabelEncoder
 
-DATA_ROOT = Path(__file__).resolve().parent / 'data'
+DATA_ROOT = Path(__file__).parent.parent.resolve()
+print(DATA_ROOT)
 
 DATASET_CONFIGS = {
     'twitch_gamers': {
-        'zip_path': DATA_ROOT / 'twitch_gamers' / 'twitch_gamers_de.zip',
-        'extract_dir': DATA_ROOT / 'twitch_gamers',
+        'zip_path': DATA_ROOT / "data"/ 'twitch_gamers' / 'twitch_gamers_de.zip',
+        'extract_dir': DATA_ROOT/ "data" / 'twitch_gamers',
         'feature_filename': 'twitch_gamers_features_de.csv',
         'edges_filename': 'twitch_gamers_edges_de.csv'
     },
     'presidential_election': {
-        'zip_path': DATA_ROOT / 'presidential_election' / 'presidential_election.zip',
-        'extract_dir': DATA_ROOT / 'presidential_election',
+        'zip_path': DATA_ROOT /'data'/ 'presidential_election' / 'presidential_election.zip',
+        'extract_dir': DATA_ROOT/'data' / 'presidential_election',
         'feature_filename': 'presidential_election_nodes.csv',
         'edges_filename': 'presidential_election_edges.csv'
     }
@@ -49,10 +50,11 @@ def save_data_obj (data_obj, dataset_name):
     """
     Saves a PyG Data object as a pytorch object.
     """
-    save_path = Path(__file__).parent/"split_data"/ dataset_name
+    save_path = DATA_ROOT/"split_data"/ dataset_name
     save_path.mkdir(parents=True, exist_ok=True)
     file_path = save_path / f"{dataset_name}_data.pt"
     torch.save(data_obj, file_path)
+    print(type(data_obj))
     print(f"Saved {dataset_name}_data.pt to {file_path}")
 
 
@@ -87,7 +89,8 @@ def load_data_object(dataset_name: str, base_dir="split_data"):
     """
     Loads a saved PyG Data object for a dataset.
     """
-    data_path = Path(__file__).parent / f"{base_dir}/{dataset_name}/{dataset_name}_data.pt"
+    data_path = DATA_ROOT/ f"{base_dir}/{dataset_name}/{dataset_name}_data.pt"
+    print("filepath:", data_path)
     data = torch.load(data_path, weights_only=False)
     return data
 
@@ -101,7 +104,7 @@ def load_model(dataset_name, model_type, split_name, model_config, device='cpu')
     else:
         print(f"Model {model_type} not found.")
 
-    model_path = Path(__file__).parent / "saved_models" / model_fname
+    model_path = DATA_ROOT / "saved_models" / model_fname
 
     input_dim = model_config['input_dim']
     hidden_dim = model_config['hidden_dim']
