344002538a63c874078eefbe8ed89304bfb05dae
Paula Demmelmayr <paula.demmel@outlook.de>
2026-01-04 12:28:23 +0100

    changed metropolitan_split to use all nodes

--- create_splits/presidential_election/metropolitan_split.py
+++ create_splits/presidential_election/metropolitan_split.py
@@ -7,10 +7,9 @@ def create_metro_split(features_df):
     """
     Create a split based on population/ density to simulate metropolitan vs non-metropolitan areas
     based on this definition: https://en.wikipedia.org/wiki/Metropolitan_statistical_area
-    TRAIN = MSAs
+    TRAIN = MSAs and µSAs
     VAL= µSAs
     TEST = Rural areas
-    leave the remaining counties out, so not all nodes are used
     split_name: split_3
     """
 
@@ -21,37 +20,21 @@ def create_metro_split(features_df):
     val_indices = np.where((population < 50000) & (population >= 10000))[0]  # µSAs
     test_nodes = np.where(population < 10000)[0]  # Rural areas
 
-    print(f"Number of total population: {len(population)}")
-    print(f"Number of MSA nodes: {len(train_nodes)}")
-
-
     rng = np.random.default_rng(seed=42)
     rng.shuffle(train_nodes)
     rng.shuffle(val_indices)
     rng.shuffle(test_nodes)
 
+    test_target = int(num_nodes * 0.2)
+    test_nodes = test_nodes[:test_target]
+
     val_target = int(num_nodes * 0.1)
     val_nodes = val_indices[:val_target]
 
     unused_val_nodes = val_indices[val_target:]
-    num_left_out = len(unused_val_nodes)
-    percent_left_out = (num_left_out / num_nodes) * 100
+    train_nodes = np.concatenate([train_nodes, unused_val_nodes])
+    #num_left_out = len(unused_val_nodes)
 
-    # 2. Verify if any nodes were lost in the initial population split (e.g. NaNs)
-    # The sum of all groups should equal num_nodes
-    total_captured = len(train_nodes) + len(val_indices) + len(test_nodes)
-    unaccounted_nodes = num_nodes - total_captured
-
-    print("-" * 30)
-    print(f"Total Nodes: {num_nodes}")
-    print(f"Validation Target (10%): {val_target}")
-    print(f"Available µSAs: {len(val_indices)}")
-    print(f"Used µSAs: {len(val_nodes)}")
-    print(f"Counties Left Out (Unused µSAs): {num_left_out} ({percent_left_out:.2f}%)")
-
-    if unaccounted_nodes > 0:
-        print(f"WARNING: {unaccounted_nodes} nodes were not categorized (possibly NaN population data).")
-    print("-" * 30)
 
     return _create_mask(num_nodes, train_nodes, val_nodes, test_nodes)
 
