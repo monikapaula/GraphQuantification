1307986bdf11f315e1cb37792da3de519a1c7450
Paula Demmelmayr <paula.demmel@outlook.de>
2026-01-28 11:11:39 +0100

    back to normal

--- models/mlp.py
+++ models/mlp.py
@@ -18,8 +18,8 @@ class MLP(nn.Module):
         self.lin2 = nn.Linear(hidden_dim, hidden_dim)
         self.dropout = nn.Dropout(p=dropout)
         self.bn1 = torch.nn.BatchNorm1d(hidden_dim)
-        self.bn2 = nn.BatchNorm1d(hidden_dim)
-        self.lin3 = nn.Linear(hidden_dim, out_dim)
+        #self.bn2 = nn.BatchNorm1d(hidden_dim)
+        #self.lin3 = nn.Linear(hidden_dim, out_dim)
 
     def forward(self, x, edge_index=None):
 
@@ -29,8 +29,8 @@ class MLP(nn.Module):
         x = self.dropout(x)
 
         x = self.lin2(x)
-        x = self.bn2(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.lin3(x)
+        #x = self.bn2(x)
+        #x = F.relu(x)
+        #x = self.dropout(x)
+        #x = self.lin3(x)
         return F.log_softmax(x, dim=-1)
